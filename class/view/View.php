44f0ec5626ef7cade277b9abf6364490cb8d0340
Dominik Borys <domborys@gmail.com>
2020-11-04 18:20:48 +0100

    Dodano metody setProperty i addArrayProperty do klasy View

--- class/view/View.php
+++ class/view/View.php
@@ -12,10 +12,10 @@ abstract class View
     protected $templateName;
     protected $templateArray = [];
     function __construct(){
-        $this->templateArray['businessName'] = BUSINESS_NAME;
-        $this->templateArray['headerButtons'] = [['text' => 'przycisk', 'url' => '#']];
-
+        $this->setProperty('businessName', BUSINESS_NAME);
+        $this->addArrayProperty('headerButtons', ['text' => 'przycisk', 'url' => '#']);
     }
+
     protected function loadFile(string $name) : string{
         $content = ""; 
         if(file_exists(HTML_PATH . $name)){
@@ -23,6 +23,27 @@ abstract class View
         }
         return $content;
     }
+    
+    //Dodaje wartość o nazwie $name do tablicy $templateArray
+    protected function setProperty(string $name, $value){
+        $this->templateArray[$name] = $value;
+    }
+
+    private function createArrayProperty(string $name){
+        if(isset($this->templateArray[$name]))
+            $this->templateArray[$name] = [$this->templateArray[$name]];
+        else
+            $this->templateArray[$name] = [];
+    }
+
+    //Dodaje wartość do tablicy o nazwie $name w $templateArray
+    protected function addArrayProperty(string $name, $value){
+        if(!is_array($this->templateArray[$name])){
+            $this->createArrayProperty($name);
+        }
+        $this->templateArray[$name][] = $value;
+    }
+
     public function output(){
         if(isset($this->templateName)){
             $templatePath = TEMPLATES_PATH.$this->templateName;
