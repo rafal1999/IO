b45e91435af133e503693c7d16084c37e9da180d
rafal1999 <ravstb4@gmail.com>
2020-11-05 20:47:05 +0100

    sql

---
+++ sql/Inserty.sql
@@ -0,0 +1,105 @@
+﻿INSERT INTO _productcategory (_idcategory,_categoryname ) Values (0,'Owoce');
+INSERT INTO _productcategory (_idcategory,_categoryname ) Values (1,'Warzywa');
+INSERT INTO _productcategory (_idcategory,_categoryname ) Values (2,'Słodycze');
+INSERT INTO _productcategory (_idcategory,_categoryname ) Values (3,'Podstawowe');
+INSERT INTO _productcategory (_idcategory,_categoryname ) Values (4,'Napoje');
+
+--klient typ 1
+
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (0,'JanKowalski','klient123',1,'Jan','Kowalski','kowal@gmail.com','123123123','Gliwice','Akademicka','1a',NULL,'44-100','2020-11-3','012345678912345');
+
+-- kierownicy typ 2
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (1,'PiotrTracz','kierownikMiodowa',2,'Piotr','Tracz','tracz@gmail.com','000111000','Gliwice','Akademicka','2a',NULL,'44-100','2020-11-3','112345678912345');
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (2,'JanNowak','kierownikSloneczna',2,'Jan','Nowak','nowak@gmail.com','000000001','Gliwice','Akademicka','3a',NULL,'44-100','2020-11-3','212345678912345');
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (3,'TadeuszNudny','kierownikWysoka',3,'Tadeusz','Nudny','nuda@gmail.com','000000010','Gliwice','Akademicka','4a',NULL,'44-100','2020-11-3','312345678912345');
+
+-- kierowcy typ 3
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (4,'PiotrekTracz','kierowcaMiodowa',3,'Piotrek','Tracz','traczek@gmail.com','100111000','Gliwice','Akademicka','2a',NULL,'44-100','2020-11-3','112345678912341');
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (5,'JanekNowak','kierowcaSloneczna',3,'Janek','Nowak','nowaczek@gmail.com','100000001','Gliwice','Akademicka','3a',NULL,'44-100','2020-11-3','212345678912341');
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (6,'TadeuszekNudny','kierowcaWysoka',3,'Tadeuszek','Nudny','nudaek@gmail.com','100000010','Gliwice','Akademicka','4a',NULL,'44-100','2020-11-3','312345678912341');
+
+-- SZEF FIRMY typ 0
+INSERT INTO _user (_iduser,_login, _password,_type,_firstname,_lastname,_email,_phone,_city,_street,_housenumber,_flatnumber,_postalcode,_lastlogin, _cardnumber)
+			Values (7,'JanuszSkąpy','admin123',0,'Janusz','Skąpy','skapy@gmail.com','100000011','Gliwice','Akademicka','0a',NULL,'44-100','2020-11-3','312345678912001');
+
+
+
+
+
+
+INSERT INTO _shop (_idproductstorage,_idShop,_name,_idmanager,_iddriver) 
+			Values (0,0,'sklepMiodowa',1,4);
+INSERT INTO _shop (_idproductstorage,_idShop,_name,_idmanager,_iddriver) 
+			Values (1,1,'sklepSloneczna',2,5);
+INSERT INTO _shop (_idproductstorage,_idShop,_name,_idmanager,_iddriver) 
+			Values (2,2,'sklepWysoka',3,6);
+
+
+
+
+
+
+
+INSERT INTO _deliverer (_iddeliverer,_name,_address,_phone) 
+			Values (0,'DostawcaOwocówiWarzyw','Kłodnicka','222333444');
+INSERT INTO _deliverer (_iddeliverer,_name,_address,_phone) 
+			Values (1,'DostawcaSłodyczy','Lubliniecka','222333111');
+INSERT INTO _deliverer (_iddeliverer,_name,_address,_phone) 
+			Values (2,'DostawcaNapojów','tarnogórska','122333111');
+INSERT INTO _deliverer (_iddeliverer,_name,_address,_phone) 
+			Values (3,'DostawcaPodstawowy','Krótka','122333111');	
+			
+
+
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (0,1,'Makaron','Makaron spaghetti Lubellato doskonały i klasyczny wybór na szybki i wartościowy obiad.',3,3.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (1,1,'Mąka','pszenna Szymanowska typ 480',3,3.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (2,1,'Ketchup łagodny','Ketchup łagodny marki Pudliszki to łagodny w smaku sos',3,2.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (3,1,'Cukier biały','Ketchup łagodny marki Pudliszki to łagodny w smaku sos',3,1.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (4,1,'Cytryny','Świeże cytryny, oferowane przez nasz sklep internetowy',0,0.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (5,1,'PEPSI Napój gazowany','Od ponad 100 lat Pepsi niesie radość i orzeźwienie na całym świecie.',4,4.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (6,1,'Kawa mielona','STARBUCKS ESPRESSO Dark Roast Kawa mielona Dark roast Bogaty smak z nutami karmelu.',4,159.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+		   Values (7,1,'Ogórki','Ogórki krótkie spod upraw szklarniowych z nowych zbiorów, świeże i pachnące wiosną.',1,2.99,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+	   Values (8,1,'Wafelek orzechowy','Wafelek z nadzieniem mleczno-nugatowym.',2,1.59,0,3,NULL);
+INSERT INTO _Product (_idproduct,_idproductstorage,_name,_description,_idcategory,_price,_discount,_iddeliverer,_picture)
+	   Values (9,1,'Czekolada Biała','Czekolada Biała  Czy wiesz jak powstaje wedlowska biała czekolada?',2,1.59,0,3,NULL);
+
+
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (0,0,'2020-12-12',100,80,NULL,10000);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (1,1,'2020-12-12',200,70,NULL,890.23);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (2,2,'2020-12-12',300,70,NULL,891.23);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (3,3,'2020-12-12',40,10,NULL,201.99);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (4,4,'2020-12-12',200,70,NULL,810.23);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (5,5,'2020-12-12',300,70,NULL,891.23);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (6,6,'2020-12-12',120,12,NULL,999);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (7,7,'2020-12-12',500,10,NULL,100000);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (8,8,'2020-12-12',300,70,NULL,700.23);
+INSERT INTO _ProductBatch(_idbatch,_idproduct,_expirydate,_basecount,_realcount,_iddelivery,_price)
+			Values (9,9,'2020-12-12',320,300,NULL,70.23);
+
+
+		   
---
+++ sql/baza.sql
@@ -0,0 +1,198 @@
+﻿
+CREATE TABLE _Session (					
+	_idSession 	  	integer PRIMARY KEY,
+	_idUser 	  	integer,                /* klucz obcy*/
+	_key	  	  	integer,                /* klucz sesji? xd */
+	_expiredTime      	time                    /* czas trwania sesji użytkownika (do jej wygaśnięcia) ? */
+);
+
+CREATE TABLE _Cart (
+	_idCart           	integer PRIMARY KEY,    /* numer identyfikacyjny przynależny do karty zamówień (koszyk) */
+	_idUser           	integer,                /* numer identyfikacyjny użytkownika */
+	_idShop           	integer                 /* numer identyfikacyjny sklepu */
+);
+
+CREATE TABLE _User (
+	_idUser          	integer PRIMARY KEY,
+	_login           	varchar(25),            /* login użytkownika */
+	_password           	varchar(25),            /* hasło użytkownika */
+	_type	        	integer,                /* typ użytkownika */
+	_firstName      	varchar(25),            /* imię użytkownika */
+	_lastName	        varchar(25),            /* nazwisko użytkowika */
+	_email			varchar(45),		/* adres email*/
+	_phone               	varchar(12),		/* numer telfonu */
+	_city            	varchar (64),           /* adres zamówienia */
+	_street           	varchar (64),           /* adres zamówienia */
+	_houseNumber            varchar (8),            /* adres zamówienia */
+	_flatNumber            	varchar (8),            /* adres zamówienia */
+	_postalCode            	varchar (8),            /* adres zamówienia */
+	_lastLogin       	date,                   /* data ostatniego zalogowania się na stronę sklepu */
+	_cardNumber      	varchar(17)             /* numer karty bankowej użytkownika */
+);
+
+
+CREATE TABLE _ProductToCart(
+	_idProduct		integer,                /* id produktu */
+	_idCart			integer,                /* id koszyka */
+	_count			integer                 /* ilość produktów do dodania */
+);
+
+CREATE TABLE _ProductStorage (
+	_idProductStorage    	integer PRIMARY KEY,    
+	_name               	varchar(25),           	/* nazwa sklepu bądź magazynu */
+	_idManager           	integer ,   		/* numer identyfikacyjny kierownika */
+	_idDriver            	integer 		/* numer identyfikacyjny kierowcy danego sklepu */
+); 
+
+CREATE TABLE _Shop(
+	_idShop              	integer PRIMARY KEY
+) INHERITS (_ProductStorage);
+
+CREATE TABLE _Purchase (
+	_idPurchase          	integer PRIMARY KEY,
+	_idShop			integer, 
+	_time               	timestamp               /* data i godzina wykonania */
+);
+
+CREATE TABLE _Order (
+	_idOrder            	integer PRIMARY KEY,
+	_idUser             	integer,                /* klucz obcy */
+	_deliveryTime           timestamp,              /* data i godzina dostarczenia zamówienia ! NIe potrzebna bo dziedziczy */
+	_type               	integer,                /* typ zamówienia ??? */
+	_firstName          	varchar(30),            /* imię zamawiającego */
+	_lastName           	varchar(30),            /* nazwisko zamawiającego */
+	_email			varchar(45),		/* adres email*/
+	_city            	varchar (64),           /* adres zamówienia */
+	_street           	varchar (64),           /* adres zamówienia */
+	_houseNumber            varchar (8),            /* adres zamówienia */
+	_flatNumber            	varchar (8),            /* adres zamówienia */
+	_postalCode            	varchar (8),            /* adres zamówienia */
+	_phone               	varchar(12),		/* numer telefonu*/
+	_delivered          	boolean                 /* status - dostarczony, niedostarczony */
+)INHERITS (_Purchase);
+CREATE TABLE _ProductToPurchase (
+	_idProduct           	integer PRIMARY KEY,
+	_idPurchase          	integer,                /* klucz obcy ? */
+	_count              	integer,                /* liczba elementów w koszyku */
+	_price               	numeric(10,2)                /* cena całościowa za produkty w koszyku */
+);
+
+CREATE TABLE _ProductCategory(
+	_idCategory          	integer PRIMARY KEY,     /* numer kategorii produktu */
+	_categoryName        	text                 	/* nazwa danej kategorii */
+);	
+
+CREATE TABLE _Product (
+	_idProduct           	integer PRIMARY KEY,
+	_idProductStorage    	integer,                /* klucz obcy ? */
+	_name               	varchar(64),            /* nazwa produktu */
+	_description		text,			/* opis produktu */
+	_idCategory          	integer,                /* numer odpowiadający za kategorię produktu 0 - jedzenie 1- agd rtv */
+	_price               	numeric(10,2),          /* cena produktu */
+	_discount            	integer,                /* ile wynosi obniżka ceny produktu */
+	_idDeliverer         	integer,                /* numer dostawcy danego produktu */
+	_picture             	varchar(200)            /* ścieżka do pliku ze zdjęciem */
+);
+
+CREATE TABLE _ProductBatch (
+	_idBatch             	integer PRIMARY KEY,
+	_idProduct           	integer,                /* klucz obcy? */
+	_expiryDate          	date,                   /* termin daty ważności produktu */
+	_baseCount           	integer,                /* zamówiona ilość produktów */
+	_realCount           	integer,                /* dostarczona ilość produktów */
+	_idDelivery          	integer,                /* numer dostawy */
+	_price               	numeric(10,2)           /* cena całego zamówienia */
+);
+
+CREATE TABLE _Warehouse(
+	_idWarehouse 		integer PRIMARY KEY
+)INHERITS (_ProductStorage);	
+
+CREATE TABLE _WarehouseProductBatch(
+	_idWarehouseBatch    	integer PRIMARY KEY,
+	_idProduct           	integer,                /* numer identyfikacyjny produktu */
+	_idWarehouseDelivery 	integer,                /* klucz obcy? */
+	_baseCount           	integer,                /* zamówiona ilość produktów */
+	_realCount           	integer                 /* dostarczona ilość produktów */
+);
+
+CREATE TABLE _Delivery (
+	_idDelivery          	integer PRIMARY KEY,
+	_idDeliverer         	integer,                /* klucz obcy? */
+	_time               	timestamp,              /* data i godzina zaplanowanej dostawy */
+	_status             	boolean                 /* status dostawy */
+);
+
+CREATE TABLE _Deliverer (
+	_idDeliverer         	integer PRIMARY KEY,
+	_name               	varchar(64),            /* nazwa dostawcy */
+	_address            	varchar(64),            /* adres zamówienia */
+	_phone               	varchar(12)             /* numer telefonu dostawcy? */
+);
+
+  CREATE TABLE _WarehouseDelivery(  
+	_idWarehouseDelivery 	integer PRIMARY KEY,
+	_idWarehouse         	integer,                /* klucz obcy? */
+	_dateOfOrder         	date,                   /* data wykonania zamówienia */
+	_dateOfDelivery      	date,                   /* data dostarczenia zamówienia */
+	_status             	boolean                 /* status zamówienia - dostarczony/niedostarczony */
+);
+
+
+/*Nadawanie Kluczy Obcych*/
+
+
+ALTER TABLE _Session
+ADD FOREIGN KEY (_idUser) REFERENCES _User (_idUser);
+
+ALTER TABLE _Cart
+ADD FOREIGN KEY (_idUser) REFERENCES _User (_idUser);
+ALTER TABLE _Cart
+ADD FOREIGN KEY (_idShop) REFERENCES _Shop(_idShop);
+----------------------------------------------------
+
+ALTER TABLE _ProductToCart
+ADD FOREIGN KEY (_idProduct) REFERENCES _Product(_idProduct);
+ALTER TABLE _ProductToCart
+ADD FOREIGN KEY (_idCart) REFERENCES _Cart(_idCart);
+----------------------------------------------------
+
+ALTER TABLE _ProductStorage
+ADD  FOREIGN KEY (_idManager) REFERENCES _User(_idUser);
+ALTER TABLE _ProductStorage
+ADD  FOREIGN KEY (_idDriver) REFERENCES _User(_idUser);
+----------------------------------------------------
+/*
+ALTER TABLE _Shop
+ADD  FOREIGN KEY (_idDriver) REFERENCES _User(_idUser);
+ALTER TABLE _Shop
+ADD  FOREIGN KEY (_idManager) REFERENCES _User(_idUser);
+*/
+----------------------------------------------------
+ALTER TABLE _Purchase
+ADD  FOREIGN KEY (_idShop) REFERENCES _Shop(_idShop);
+----------------------------------------------------
+
+ALTER TABLE _Product
+ADD  FOREIGN KEY (_idCategory) REFERENCES _ProductCategory(_idCategory);
+ALTER TABLE _Product
+ADD  FOREIGN KEY (_idDeliverer) REFERENCES _deliverer(_idDeliverer);
+/*ALTER TABLE _Product
+ADD  FOREIGN KEY (_idProductStorage) REFERENCES _ProductStorage(_idProductStorage);*/ -- nie używać 
+
+
+----------------------------------------------------
+
+ALTER TABLE _ProductBatch
+ADD  FOREIGN KEY (_idProduct) REFERENCES _Product(_idProduct);
+----------------------------------------------------
+
+ALTER TABLE _WarehouseProductBatch
+ADD  FOREIGN KEY (_idProduct) REFERENCES _Product(_idProduct);
+
+ALTER TABLE _WarehouseProductBatch
+ADD  FOREIGN KEY (_idWarehouseDelivery) REFERENCES _WarehouseDelivery(_idWarehouseDelivery);
+----------------------------------------------------
+
+ALTER TABLE _Delivery
+ADD  FOREIGN KEY (_idDeliverer) REFERENCES _Deliverer(_idDeliverer);
